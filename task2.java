0f54a33e119221807781eada31cd4cc1ffad5422
vlasenkova <asyicheva1@mail.ru>
2022-12-12 13:02:10 +0300

    task 2/

--- task2.java
+++ task2.java
@@ -1,9 +1,9 @@
 // Вывести все простые числа от 1 до 1000
 public class task2 {
     public static void main(String[] args) {
-    int numberber = 1000;
+    int number = 1000;
     int count = 0;
-    for (int i = 2; i < numberber; i++) {
+    for (int i = 2; i < number; i++) {
         boolean a = true;
         for (int j = 2; j < i; j++) {
             if (i % j == 0) {
